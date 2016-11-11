550fd5a17fc6ca4a95ea750d0325fd70dd0bcfc4
Jorge Ayres Pereira <jgcaap@gmail.com>
2016-11-11 16:59:30 +0100

    Change kernel GCC compiler

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,7 +51,7 @@ BOARD_DTBTOOL_ARGS := -2
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_CONFIG := cyanogenmod_bacon_defconfig
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8974
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.9/bin/arm-eabi-
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
